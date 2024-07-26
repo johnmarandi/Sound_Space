5e1102e89360a754df1621ffb200614978f49704
Himanshu John Marandi <71967840+johnmarandi@users.noreply.github.com>
2024-07-26 10:33:15 +0530

    Update DisplayAlbum.jsx

--- src/components/DisplayAlbum/DisplayAlbum.jsx
+++ src/components/DisplayAlbum/DisplayAlbum.jsx
@@ -19,7 +19,7 @@ const DisplayAlbum = () => {
                     <p>Playlist</p>
                     <h2 className='text-5xl font-bold mb-4 md:text-7xl'>{albumData.name}</h2>
                     <h4>{albumData.desc}</h4>
-                    <p className='mt-1'><img className='inline-block w-5' src={assets.spotify_logo} alt="" /> <b>Spotify</b> • 1,323,154 likes • <b>50 songs,</b> about 2 hr 30 min</p>
+                    <p className='mt-1'><img className='inline-block w-5' src={assets.speaker.png} alt="" /> <b>Spotify</b> • 1,323,154 likes • <b>50 songs,</b> about 2 hr 30 min</p>
                 </div>
             </div>
             <div className='grid grid-cols-3 sm:grid-cols-4 mt-10 mb-4 pl-2 text-[#a7a7a7]'>
